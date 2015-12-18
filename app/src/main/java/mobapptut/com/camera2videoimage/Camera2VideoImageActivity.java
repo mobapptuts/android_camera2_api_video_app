f03be2a2bfad01d950971bd20d645f9bb6df6263
mobapptuts <nige.henshaw@gmail.com>
2015-12-18 21:01:34 +0900

    Initial commit - Including the xml code for adding ImageButton views to the layout

---
+++ app/src/main/java/mobapptut/com/camera2videoimage/Camera2VideoImageActivity.java
@@ -0,0 +1,13 @@
+package mobapptut.com.camera2videoimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Camera2VideoImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera2_video_image);
+    }
+}
