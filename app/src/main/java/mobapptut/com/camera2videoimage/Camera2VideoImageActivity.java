7a774ed4782be03bed9f999f60cd18758899c899
mobapptuts <nige.henshaw@gmail.com>
2015-12-21 11:12:37 +0900

    Implemented full-screen sticky immersive mode

--- app/src/main/java/mobapptut/com/camera2videoimage/Camera2VideoImageActivity.java
+++ app/src/main/java/mobapptut/com/camera2videoimage/Camera2VideoImageActivity.java
@@ -2,6 +2,7 @@ package mobapptut.com.camera2videoimage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Camera2VideoImageActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera2_video_image);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocas) {
+        super.onWindowFocusChanged(hasFocas);
+        View decorView = getWindow().getDecorView();
+        if(hasFocas) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
     }
 }
