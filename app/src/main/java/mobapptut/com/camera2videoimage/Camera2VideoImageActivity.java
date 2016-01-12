22ebaa7fe2ca7ad891150c06e086e1f6db1ba99b
mobapptuts <nige.henshaw@gmail.com>
2016-01-12 11:50:29 +0900

    Finding the cameraId

--- app/src/main/java/mobapptut/com/camera2videoimage/Camera2VideoImageActivity.java
+++ app/src/main/java/mobapptut/com/camera2videoimage/Camera2VideoImageActivity.java
@@ -1,7 +1,11 @@
 package mobapptut.com.camera2videoimage;
 
+import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
@@ -14,7 +18,7 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
     private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-            Toast.makeText(getApplicationContext(), "TextureView is available", Toast.LENGTH_SHORT).show();
+            setupCamera(width, height);
 
         }
 
@@ -52,6 +56,7 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
             mCameraDevice = null;
         }
     };
+    private String mCameraId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +71,7 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
         super.onResume();
 
         if(mTextureView.isAvailable()) {
-
+            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
         } else {
             mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
         }
@@ -90,6 +95,23 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    }
+
+    private void setupCamera(int width, int height) {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for(String cameraId : cameraManager.getCameraIdList()){
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+                if(cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
+                        CameraCharacteristics.LENS_FACING_FRONT){
+                    continue;
+                }
+                mCameraId = cameraId;
+                return;
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
     }
 
